c213da7b1180e24819fa38b7f34df43cb1ed75e0
sbcreates <drawsarahb@gmail.com>
2017-08-17 18:10:11 -0400

    Finished exercise

--- ruby_doc-research.rb
+++ ruby_doc-research.rb
@@ -26,34 +26,64 @@
 
 #ARRAY
 
-#1 first -
+#1 first - returns the first element of an array. If the array is empty, it will return nil.
+a = [ "q", "r", "s", "t" ]
+a.first     #=> "q"
+a.first(2)  #=> ["q", "r"]
 
-#2 delete_at -
+#2 delete_at - deletes element at the specified index. should return specified element, or nil is the index is out of range.
+a = ["ant", "bat", "cat", "dog"]
+a.delete_at(2)    #=> "cat"
+a                 #=> ["ant", "bat", "dog"]
+a.delete_at(99)   #=> nil
 
-#3 delete -
+#3 delete - deletes all items from aray that are equal to index or value given to delete.
+a = [ "a", "b", "b", "b", "c" ]
+a.delete("b")                   #=> "b"
+a                               #=> ["a", "c"]
+a.delete("z")                   #=> nil
+a.delete("z") { "not found" }   #=> "not found"
 
-#4 pop -
-
-
+#4 pop - returns last element from array and returns it, or nil if array is empty.
+a = [ "a", "b", "c", "d" ]
+a.pop     #=> "d"
+a.pop(2)  #=> ["b", "c"]
+a         #=> ["a"]
 
 #HASH
 
-#1 to_a
+#1 to_a - converts hash to a nested array of [key, value] arrays.
+h = { "c" => 300, "a" => 100, "d" => 400, "c" => 300  }
+h.to_a   #=> [["c", 300], ["a", 100], ["d", 400]]
 
-#2 has_key?
+#2 has_key? - returns true if the given key is present in the hash.
+h = { "a" => 100, "b" => 200 }
+h.has_key?("a")   #=> true
+h.has_key?("z")   #=> false
 
-#3 has_value?
+#3 has_value? - returns true is the given value is present for some key in hash.
+h = { "a" => 100, "b" => 200 }
+h.has_value?(100)   #=> true
+h.has_value?(999)   #=> false
 
 
 
 #TIME
 
-#1 now -
+#1 now - creates a new time object for the *current* time.
+Time.now            #=> 2009-06-24 12:39:54 +0900
 
 
 
 #FILE
 
-#1 exist? -
+#1 exist? - return true if the named file exists .
 
-#2 extname -
+#2 extname - returns the extenstion of the file. An empty string is returned when the period is the last character in path.
+File.extname("test.rb")         #=> ".rb"
+File.extname("a/b/d/test.rb")   #=> ".rb"
+File.extname(".a/b/d/test.rb")  #=> ".rb"
+File.extname("foo.")            #=> ""
+File.extname("test")            #=> ""
+File.extname(".profile")        #=> ""
+File.extname(".profile.sh")     #=> ".sh"
